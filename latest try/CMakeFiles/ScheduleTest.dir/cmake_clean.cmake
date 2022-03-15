cf135fe86873e57f045d47e465a99d9a9e004df7
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2022-03-15 00:16:04 -0500

    Add files via upload

---
+++ latest try/CMakeFiles/ScheduleTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ScheduleTest.dir/ScheduleTest.cpp.o"
+  "CMakeFiles/ScheduleTest.dir/ScheduleTest.cpp.o.d"
+  "ScheduleTest"
+  "ScheduleTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ScheduleTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
